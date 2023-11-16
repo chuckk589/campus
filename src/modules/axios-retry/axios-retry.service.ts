3b9feb03c283e44db7a441c56165aa2f5adcd4d8
andrew <chuckk589@gmail.com>
2023-11-16 16:55:54 +0300

    https fix

--- src/modules/axios-retry/axios-retry.service.ts
+++ src/modules/axios-retry/axios-retry.service.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosRequestConfig, Method, AxiosResponse } from 'axios';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
+import https from 'https';
 
 @Injectable()
 export class AxiosRetryService {
@@ -17,13 +18,16 @@ export class AxiosRetryService {
         .axiosRef({
           method: options.method,
           url: options.url,
+          httpsAgent: new https.Agent({
+            rejectUnauthorized: false,
+          }),
           data: options.data,
           ...options,
         })
         .then((response) => resolve(response))
         .catch((error) => {
-          this.logger.info(`Request failed with status code ${error.response.status}, retries left: ${3 - retries}`);
-          if (error.response.status === 429) {
+          this.logger.info(`Request failed with status code ${error?.response?.status}, retries left: ${3 - retries}`);
+          if (error?.response?.status === 429) {
             if (retries < 3) {
               retries++;
               setTimeout(() => {
