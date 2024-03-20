596229fc469ce8de3f6ed84d989ecb020ab5b03a
andrew <chuckk589@gmail.com>
2024-03-20 15:36:15 +0300

    adminer bash

---
+++ adminer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -e -p "new ip: " ip
+#find local ip
+local_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' campus-adminer-1)
+#find rule by comment 
+rule=$(ufw status numbered | grep adminer | awk -F'[][]' '{print $2}')
+#force delete old rule
+ufw --force delete $rule
+#add new rule
+ufw route allow proto tcp from $ip to $local_ip port 8080 comment 'adminer'
