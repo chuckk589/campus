cbd4da3dfa1a5a5ed7bb43fccd38c35eff7c8541
andrew <chuckk589@gmail.com>
2024-03-24 03:17:23 +0300

    update

--- ufw.sh
+++ ufw.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-docker events --filter container=campus-app-1 --format '{{.Status}}' |
-while read line;
-do
-    if [[ ${line} = "start" ]];
-    then
-        ufw-docker allow campus-app-1 80;
-    fi ;
-done
+sleep 1m
+ufw-docker allow campus-app-1 80
