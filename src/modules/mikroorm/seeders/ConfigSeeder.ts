42922b35b0da33681b7769d0ee5aec54618e7e6a
andrew <chuckk589@gmail.com>
2024-06-02 13:43:43 +0300

    chore: create superadmin in config seeder

--- src/modules/mikroorm/seeders/ConfigSeeder.ts
+++ src/modules/mikroorm/seeders/ConfigSeeder.ts
@@ -1,12 +1,10 @@
 /* eslint "prettier/prettier": ['error', { printWidth: 100 }]*/
 import { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
-import { Config } from '../entities/Config';
 
 export class ConfigSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
     const values = [
-      `("ADMIN_PASSCODE", "$2a$12$DC3oYahZU5pZIFbFMhminuEnlSPXb4coN8AfEOkRpcOTpefSXSdAu", "Пароль администратора", "secret")`,
       `("VERSION", "0.0.1", "Текущая версbия приложения", NULL)`,
       `("QUESTION_TIME", "4-10", "Время на ответ на вопрос мин - макс, в секундах, через дефис", NULL)`,
       `("HOSTNAME", "http://195.161.114.112:3000", "Адрес сервера, используется при генерации расширения", NULL)`,
@@ -19,7 +17,13 @@ export class ConfigSeeder extends Seeder {
       .execute(
         `INSERT INTO config (name, value, description, category) VALUES ${values.join(
           ',',
-        )} ON DUPLICATE KEY UPDATE name=name`,
+        )} ON DUPLICATE KEY UPDATE name=name;`,
+      );
+    //password: 123
+    await em
+      .getConnection()
+      .execute(
+        `INSERT INTO owner (created_at, updated_at, role, username, password, email, credentials) VALUES (now(), now(), 2, 'superadmin', '$2a$12$tUh2oW4.kL8Vxsfm0Wp8J.t4/drRqAc0824e0sPltyGJ8dfI6kKx.', 'superadmin@admin.com', 'Vasya Pupkin') ON DUPLICATE KEY UPDATE username=username;`,
       );
   }
 }
