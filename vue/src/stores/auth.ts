e089bdc8d1b8b46b56bf0fe5388674cc08a77231
andrew <chuckk589@gmail.com>
2024-06-02 13:47:31 +0300

    chore: added registration page

---
+++ vue/src/stores/auth.ts
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia';
+import { router } from '../router';
+import axiosInstance from '@/utils/axios';
+export const useAuthStore = defineStore({
+  id: 'auth',
+  state: () => ({
+    // initialize state from local storage to enable user to stay logged in
+    /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+    // @ts-ignore
+    user: JSON.parse(localStorage.getItem('user')),
+    returnUrl: null,
+  }),
+  actions: {
+    async login(username: string, password: string) {
+      const user = await axiosInstance({
+        method: 'POST',
+        url: '/auth/login',
+        data: { username, password },
+      }).then((res) => res.data);
+      // update pinia state
+      this.user = user;
+      // store user details and jwt in local storage to keep user logged in between page refreshes
+      localStorage.setItem('user', JSON.stringify(user));
+      // redirect to previous url or default to home page
+      router.push(this.returnUrl || '/main/users');
+    },
+    async register(username: string, password: string) {
+      const user = await axiosInstance({
+        method: 'POST',
+        url: '/auth/register',
+        data: { username, password },
+      }).then((res) => res.data);
+      // update pinia state
+      this.user = user;
+      // store user details and jwt in local storage to keep user logged in between page refreshes
+      localStorage.setItem('user', JSON.stringify(user));
+      // redirect to previous url or default to home page
+      router.push(this.returnUrl || '/main/users');
+    },
+    logout() {
+      this.user = null;
+      localStorage.removeItem('user');
+      router.push('/auth/login');
+    },
+  },
+});
---
+++ vue/src/views/authentication/auth/LoginPage.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import AuthLogin from '../authForms/AuthLogin.vue';
+</script>
+
+<template>
+  <v-row class="h-screen" no-gutters>
+    <!---Left Part-->
+    <v-col cols="12" class="d-flex align-center bg-lightprimary">
+      <v-container>
+        <div class="pa-7 pa-sm-12">
+          <v-row justify="center">
+            <v-col cols="12" lg="10" xl="6" md="7">
+              <v-card elevation="0" class="loginBox">
+                <v-card variant="outlined">
+                  <v-card-text class="pa-9">
+                    <!---Left Part Logo -->
+                    <v-row>
+                      <v-col cols="12" class="text-center">
+                        <h2 class="text-secondary text-h2 mt-8">
+                          Hi, Welcome Back
+                        </h2>
+                        <h4 class="text-disabled text-h4 mt-3">
+                          Enter your credentials to continue
+                        </h4>
+                      </v-col>
+                    </v-row>
+                    <!---Left Part Logo -->
+
+                    <!---Left Part Form-->
+                    <AuthLogin />
+                    <!---Left Part Form-->
+                  </v-card-text>
+                </v-card>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
+      </v-container>
+    </v-col>
+    <!---Left Part-->
+  </v-row>
+</template>
+<style lang="scss">
+.loginBox {
+  max-width: 475px;
+  margin: 0 auto;
+}
+.text-h2 {
+  font-size: 1.5rem !important;
+  font-weight: 700;
+  line-height: 2.5rem;
+  letter-spacing: -0.0083333333em !important;
+}
+.text-h4 {
+  font-size: 1rem !important;
+  font-weight: 600;
+  line-height: 1.5rem;
+  letter-spacing: 0.0073529412em !important;
+}
+.text-h5 {
+  font-size: 0.875rem !important;
+  font-weight: 500;
+  line-height: 1.2rem;
+  letter-spacing: normal !important;
+}
+.v-text-field input {
+  font-size: 0.875rem;
+}
+.v-label {
+  font-size: 0.975rem;
+}
+.v-btn {
+  text-transform: capitalize;
+  letter-spacing: 0;
+}
+.v-card--variant-outlined {
+  border-color: #d0d0d05c;
+}
+.bg-lightprimary {
+  background-color: rgb(238, 242, 246) !important;
+}
+.text-secondary {
+  color: rgb(94, 53, 177) !important;
+}
+.bg-secondary {
+  background-color: rgb(94, 53, 177) !important;
+  color: rgb(238, 242, 246) !important;
+}
+</style>
