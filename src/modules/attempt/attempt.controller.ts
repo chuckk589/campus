656fc2a6b4c88ed583b217df64f5dee6b2ae571a
andrew <chuckk589@gmail.com>
2024-06-13 23:18:16 +0300

    refactor: commented block

--- src/modules/attempt/attempt.controller.ts
+++ src/modules/attempt/attempt.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
-import { Put, Req } from '@nestjs/common/decorators';
+import { Put, Req, UseInterceptors } from '@nestjs/common/decorators';
 import { UpdateAnswerDto } from '../answers/dto/update-answer.dto';
 import { LoggedInGuard } from '../auth/guards/loggedin.guard';
 import { AttemptService } from './attempt.service';
@@ -8,6 +8,9 @@ import { UpdateAttemptDto } from './dto/update-attempt.dto';
 import { IServerSideGetRowsRequest } from 'src/types/agGridTypes';
 import { RequestWithUser } from 'src/types/interfaces';
 import { Roles } from '../auth/guards/role.guard';
+import { RetrieveAttemptDto } from './dto/retrieve-attempt.dto';
+import { UserSanitizeInterceptor } from 'src/common/userSanitizeInterceptor';
+import { RetrieveAttemptAnswerDto } from './dto/retrieve-attempt-answer.dto';
 
 @Controller({ version: '1', path: 'attempt' })
 @UseGuards(LoggedInGuard)
@@ -15,6 +18,7 @@ export class AttemptController {
   constructor(private readonly attemptService: AttemptService) {}
 
   @Post('/load')
+  @UseInterceptors(new UserSanitizeInterceptor(RetrieveAttemptDto, 'rows'))
   lazyload(@Req() req: RequestWithUser, @Body() body: IServerSideGetRowsRequest) {
     return this.attemptService.lazyload(body, req.user);
   }
@@ -25,10 +29,10 @@ export class AttemptController {
     return this.attemptService.update(+id, updateAttemptDto);
   }
 
-  @Put('/answer/:id')
-  updateAnswer(@Param('id') id: string, @Body() updateAttemptAnswerDto: UpdateAttemptAnswerDto) {
-    return this.attemptService.updateAnswer(+id, updateAttemptAnswerDto);
-  }
+  // @Put('/answer/:id')
+  // updateAnswer(@Req() req: RequestWithUser, @Param('id') id: string, @Body() updateAttemptAnswerDto: UpdateAttemptAnswerDto) {
+  //   return this.attemptService.updateAnswer(req.user, +id, updateAttemptAnswerDto);
+  // }
 
   @Get('/pattern/:id/ai')
   getAiAnswer(@Param('id') attemptAnswerId: string) {
@@ -36,6 +40,7 @@ export class AttemptController {
   }
 
   @Put('/pattern/:id')
+  @UseInterceptors(new UserSanitizeInterceptor(RetrieveAttemptAnswerDto))
   updatePattern(@Req() req: RequestWithUser, @Param('id') attemptAnswerId: string, @Body() updateAnswerDto: UpdateAnswerDto) {
     return this.attemptService.updatePattern(req.user, +attemptAnswerId, updateAnswerDto);
   }
