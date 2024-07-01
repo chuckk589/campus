a189e86b0ccacfb8e3a83c89a5b804132cf3ccb1
andrew <chuckk589@gmail.com>
2024-07-01 18:26:17 +0300

    chore: permissions type changed from string to enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
 export const LISTENERS_METADATA = 'LISTENERS_METADATA';
 export const REDIS = 'REDIS';
 export const REDIS_STORE = 'REDIS_STORE';
+
+//permissions
+export enum PERMISSIONS {
+  QUIZ_EDIT_OWN = 'quiz_edit_own',
+  QUIZ_VIEW_STATE = 'quiz_view_state',
+}
