6f4f6b48ea1af3222af882b4b97dde9cae34709e
andrew <chuckk589@gmail.com>
2024-07-01 18:44:20 +0300

    fix: accept array of all question states instead of correct only

--- src/modules/quiz/dto/finish-quiz.dto.ts
+++ src/modules/quiz/dto/finish-quiz.dto.ts
@@ -1,12 +1,14 @@
 import { IsArray, IsNumberString, IsObject, IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { QuestionState } from 'src/modules/mikroorm/entities/QuizAnswerState';
 
 export class summaryData {
   [key: string]: string;
 }
 export class FinishQuizDto {
-  @IsString({ each: true })
-  correctQuestions!: string[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  questions!: { value: string; state: QuestionState }[];
 
   @IsObject()
   @Type(() => summaryData)
