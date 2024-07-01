52666643af2513b8aba646c0f2fe189ff0d66a72
andrew <chuckk589@gmail.com>
2024-07-01 18:45:07 +0300

    test: removed unused classes

--- test/utils/seeders/restrictions.ts
+++
@@ -1,8 +0,0 @@
-import { MikroORM } from '@mikro-orm/core';
-
-export default async function Seed(orm: MikroORM) {
-  await orm.em.getConnection().execute(
-    `INSERT INTO user_restriction (\`id\`, \`created_at\`, \`updated_at\`, \`user_id\`, \`login\`, \`name\`) VALUES
-    (1,	'2024-01-08 21:07:21',	'2024-01-08 21:07:21',	'32839',	'220365',	'Антонина Лебедева');`,
-  );
-}
