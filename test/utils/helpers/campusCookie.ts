6cdacc76ff4111d782090f9a8251df36767eca78
andrew <chuckk589@gmail.com>
2024-07-02 19:31:09 +0300

    test: sync to [55d58c2]

--- test/utils/helpers/campusCookie.ts
+++ test/utils/helpers/campusCookie.ts
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
-export async function getCampusCookie() {
+import fs from 'fs';
+async function getCampusCookie() {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto('https://campus.fa.ru/login/index.php');
@@ -12,4 +13,24 @@ export async function getCampusCookie() {
   await browser.close();
   console.log('Cookie acquired: ', cookies);
   return `${cookies.name}=${cookies.value}`;
+}
+
+export async function loadExisting(): Promise<string> {
+  try {
+    const cookie = fs.readFileSync('./test/utils/helpers/cookie.txt', 'utf8');
+    //check if not expired 1hr
+    if (Date.now() - parseInt(cookie.split('\n')[1]) > 3600000) {
+      console.log('Cookie expired, getting new one');
+      const newCookie = await getCampusCookie();
+      fs.writeFileSync('./test/utils/helpers/cookie.txt', newCookie + '\n' + Date.now());
+      return newCookie;
+    }
+    return cookie.split('\n')[0];
+  } catch (e) {
+    const cookie = await getCampusCookie();
+    fs.writeFileSync('./test/utils/helpers/cookie.txt', cookie + '\n' + Date.now());
+    return cookie;
+  } finally {
+    console.log('Cookie loaded');
+  }
 }
